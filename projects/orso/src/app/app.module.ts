16cc6479e4c9fe77f9b0527505f9b255f5703d6c
PRED-ONE\al5mo <al.5.montagni@gmail.com>
2023-07-28 22:13:15 +0200

    orso carousel

--- projects/orso/src/app/app.module.ts
+++ projects/orso/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { NgbCarouselModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { CarouselOneComponent } from './components/carousel-one/carousel-one.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    CarouselOneComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule,
+    NgbModule,
+    NgbCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/orso/src/app/components/carousel-one/carousel-one.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarouselOneComponent } from './carousel-one.component';
+
+describe('CarouselOneComponent', () => {
+  let component: CarouselOneComponent;
+  let fixture: ComponentFixture<CarouselOneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarouselOneComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CarouselOneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
